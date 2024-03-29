321ea3fe01756d8fd6ddec57b1d309a50ff85cfe
Your Name <your_email@example.com>
2024-03-29 15:20:27 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Navigation from './components/Navigation'
+
+function App() {
+
+  return (
+  <>
+  <Navigation/>
+  <h1 className='text-center mt-32 bg-blue-500'>DEMO</h1>
+  </>
+  )
+}
+
+export default App
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navigation = () => {
+  return (
+    <nav className="bg-blue-500 p-4 fixed top-0 w-full flex justify-center text-center">
+    <div className="container mx-auto">
+      <span className="text-white font-bold text-lg">TEST APP</span>
+    </div>
+  </nav>
+  )
+}
+
+export default Navigation
